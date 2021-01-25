a291199f3991bfb24da7815c68eb0e5d1051bea8
Viraj Kadakia <virajkadakia@berkeley.edu>
2021-01-24 16:54:24 -0800

    US-1 : SQL Setup
    Initialized AWS RDS for our database. Created a javascript script to create an initial database.
    mysql is now also added as a package in package.json to be downloaded through docker

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,12 @@
 const express = require('express');
-const app = express();
 const bodyParser = require('body-parser');
 const cors = require('cors');
+
+const app = express();
 const PORT = 4000;
 
 app.use(cors());
+// parse requests of content-type: application/json
 app.use(bodyParser.json());
 
 app.listen(PORT, function() {
