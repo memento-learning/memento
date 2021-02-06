868abb50c6c6e2598cb102bbdd40b1a80e2bbc36
Ammar Husain <ammuhusain98@gmail.com>
2021-02-06 12:11:26 -0800

    Added basic relations for data model

--- db/sql/V1__init_db.sql
+++ db/sql/V1__init_db.sql
@@ -1,12 +1,47 @@
 CREATE TABLE User(
-    user_id int PRIMARY KEY
+    PRIMARY KEY (user_id),
+    user_id INT NOT NULL AUTO_INCREMENT,
+    username VARCHAR(20),
+    password VARCHAR(20)
 );
 
 CREATE TABLE Deck (
-    deck_id int PRIMARY KEY
+    PRIMARY KEY(deck_id),
+    deck_id INT NOT NULL AUTO_INCREMENT, 
+    name VARCHAR(20)
 );
-
 
 CREATE TABLE UserDeck (
-    user_id int
+    user_id INT NOT NULL,
+    deck_id INT NOT NULL,
+    FOREIGN KEY (user_id) REFERENCES User(user_id),
+    FOREIGN KEY (deck_id) REFERENCES Deck(deck_id),
+    UNIQUE (user_id, deck_id)
 );
+
+CREATE TABLE Item (
+    PRIMARY KEY(item_id),
+    item_id INT NOT NULL AUTO_INCREMENT,
+    deck_id INT NOT NULL,
+    FOREIGN KEY (deck_id) REFERENCES Deck(deck_id),
+    question VARCHAR(100),
+    answer VARCHAR(100)
+);
+
+CREATE TABLE UserItem (
+    user_id INT NOT NULL,
+    item_id INT NOT NULL,
+    num_reviewed INT DEFAULT 0,
+    FOREIGN KEY (user_id) REFERENCES User(user_id),
+    FOREIGN KEY (item_id) REFERENCES Item(item_id),
+    UNIQUE (user_id, item_id)
+);
+
+INSERT INTO User (username, password)
+VALUES ('gathass', 'iamgathass');
+
+INSERT INTO Deck (name)
+VALUES ('spanish');
+
+INSERT INTO UserDeck (user_id, deck_id)
+VALUES (1, 1);
