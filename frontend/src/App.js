152d25f74d756b08c6ac50b592bbc3e044c61c1d
Ammar Husain <ammuhusain98@gmail.com>
2021-02-06 14:32:31 -0800

    Added linting instructions and fixed lint rules

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,19 +1,18 @@
 import logo from './logo.svg';
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import './App.css';
 
 class App extends Component {
-
   state = {
-    message: "Hello Gathass from UI!"
+    message: 'Hello Gathass from UI!',
   }
 
   componentDidMount() {
     fetch('/api/hello')
-      .then(res => res.text())
-      .then((data) => {
-        this.setState({ message: data })
-      })
+        .then((res) => res.text())
+        .then((data) => {
+          this.setState({message: data});
+        });
   }
   render() {
     return (
