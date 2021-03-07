73990e30140d57bb683d1787dcb3a1c9b5f913e0
tejas <manjunathtej4s@gmail.com>
2021-03-06 19:24:17 -0800

    this branch is getting a timeout issue

---
+++ frontend/src/components/App.js
@@ -0,0 +1,18 @@
+import DeckList from './DeckList';
+import DeckPreview from './DeckPreview';
+import Deck from './Deck';
+import {BrowserRouter, Route, Switch} from 'react-router-dom';
+
+function App() {
+  return (
+    <main>
+      <Switch>
+        <Route path="/" component={DeckList} exact />
+        <Route path="/Deck" component={Deck} exact />
+        <Route component={Error} />
+      </Switch>
+    </main>
+  );
+}
+
+export default App;
