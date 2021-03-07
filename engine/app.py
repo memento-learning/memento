55d094da49ac92f6e24dcb20813a4df644ecbfdc
nikswam <nikswam@gmail.com>
2021-03-06 18:41:37 -0800

    Functioning engine server returning hi

---
+++ engine/app.py
@@ -0,0 +1,10 @@
+from flask import Flask
+from flask_cors import CORS
+from routes import routes_blueprint 
+
+def create_app():
+    app = Flask(__name__)
+    app.debug = True
+    CORS(app, resources={r"/*": {"origins": "http://localhost:5000"}})
+    app.register_blueprint(routes_blueprint)
+    return app
